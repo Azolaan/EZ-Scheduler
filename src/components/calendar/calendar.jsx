119437cacb601285cb04b8cdde26cf1f57145f4d
Alex Lo <loa@mcmaster.ca>
2020-11-04 18:53:25 -0500

    add calendar component template

---
+++ src/components/calendar/calendar.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+import "./calendar.css"
+
+class CalendarComponent extends React.Component {
+    render() {
+        return <div />
+    }
+}
+
+export { CalendarComponent, CalendarComponent as default }
