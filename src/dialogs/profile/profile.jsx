f1c70fc1f548df01bb7c542fbf12f048799a1fdc
Lawrence Chung <chungl1@mcmaster.ca>
2020-11-12 21:48:18 -0500

    Added warning dialogue if attempted to close with unsaved changes

--- src/dialogs/profile/profile.jsx
+++ src/dialogs/profile/profile.jsx
@@ -5,6 +5,8 @@ import { Button } from '@rmwc/button'
 
 import "./profile.css"
 
+// remind of unsaved changes if attempted to close and editState = true
+
 class Profile extends React.Component {
     constructor(props) {
         super(props)
@@ -18,48 +20,42 @@ class Profile extends React.Component {
     }
 
     callBackFn = (val) => {
-        var curState
+        var curState, newState
+        console.log(val)
         switch(val) {
             case "Account Information":
-                curState = this.state.accInfoEditState
-                this.setState({accInfoEditState : !curState})
+                this.setState((state) => ({ accInfoEditState: !state.accInfoEditState }), () => console.log("accinfoeditstate:", this.state.accInfoEditState))
                 break
             case "Personal Information":
-                curState = this.state.personalInfoEditState
-                this.setState({personalInfoEditState : !curState})
+                this.setState((state) => ({ personalInfoEditState: !state.personalInfoEditState }), () => console.log("personalinfoeditstate:", this.state.personalInfoEditState))
                 break
             case "Bio":
-                curState = this.state.bioEditState
-                this.setState({bioEditState : !curState})
+                this.setState((state) => ({ bioEditState: !state.bioEditState }), () => console.log("bioeditstate:", this.state.bioEditState))
                 break
         }
-        if(this.state.accInfoEditState || this.state.personalInfoEditState || this.bioEditState) {
-            this.setState({editState : true})
-        } else {
-            this.setState({editState : false})
-        }
-    }
 
-    closeButtonSelector = () => {
-        if(this.state.editState) {
-            return ("Save")
-        } else {
-            return ("Close")
-        }
     }
 
     closeDialog = () => {
-        this.setState({
-            isOpen : false,
-            accInfoEditState : false,
-            personalInfoEditState : false,
-            bioEditState : false,
-            editState : false
-        })
+        console.log(this.state.editState)
+        if(this.state.editState){
+            return(
+                <>
+                <WarningDialog />
+                </>
+            )
+        } else {
+            this.setState({
+                isOpen : false,
+                accInfoEditState : false,
+                personalInfoEditState : false,
+                bioEditState : false,
+                editState : false
+            })
+        }
     }
 
     render() {
-        let buttonText = this.closeButtonSelector()
         return (
             <div>
                 <div>
@@ -68,18 +64,32 @@ class Profile extends React.Component {
                     onClose={() => this.setState({isOpen : false})}>
                         <DialogTitle style={{textAlign: "center"}}>Profile<br />Picture here<br />{this.props.userInfo["name"]}</DialogTitle>
                         <DialogContent>
-                            <Edit section="Account Information" callBackFn={this.callBackFn.bind(this)}/><br />
-                            Username (ID): <div class="right-align">{this.props.userInfo.userName}</div><br />
-                            Password: <div class="right-align">{this.props.userInfo.password}</div><br /><br />
-                            <Edit section="Personal Information" callBackFn={this.callBackFn.bind(this)}/><br />
-                            School: <div class="right-align">{this.props.userInfo.school}</div><br />
-                            Major: <div class="right-align">{this.props.userInfo.major}</div><br />
-                            Year: <div class="right-align">{this.props.userInfo.year}</div><br /><br />
-                            <Edit section="Bio" callBackFn={this.callBackFn.bind(this)}/><br />
+                            <Edit
+                            section="Account Information"
+                            callBackFn={this.callBackFn.bind(this)}
+                            editState={this.state.accInfoEditState}
+                            /><br />
+                            Username (ID): <div className="right-align">{this.props.userInfo.userName}</div><br />
+                            Password: <div className="right-align">{this.props.userInfo.password}</div><br /><br />
+                            <Edit
+                            section="Personal Information"
+                            callBackFn={this.callBackFn.bind(this)}
+                            editState={this.state.personalInfoEditState}
+                            /><br />
+                            School: <div className="right-align">{this.props.userInfo.school}</div><br />
+                            Major: <div className="right-align">{this.props.userInfo.major}</div><br />
+                            Year: <div className="right-align">{this.props.userInfo.year}</div><br /><br />
+                            <Edit
+                            section="Bio"
+                            callBackFn={this.callBackFn.bind(this)}
+                            editState={this.state.bioEditState}
+                            /><br />
                             {this.props.userInfo.bio}
                         </DialogContent>
                         <DialogActions style={{textAlign: "center"}}>
-                            <Button onClick={() => this.setState({isOpen: false})}>{buttonText}</Button>
+                            <WarningDialog 
+                            state={this.state}
+                            callBackFn={this.closeDialog.bind(this)}/>
                         </DialogActions>
                     </Dialog>
                 </div>
@@ -91,21 +101,78 @@ class Profile extends React.Component {
     }
 }
 
+class WarningDialog extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isOpen : false
+        }
+    }
+
+    editState = () => {
+        if(!this.props.state.accInfoEditState && !this.props.state.bioEditState && !this.props.state.personalInfoEditState) {
+            return (false)
+        } else {
+            return (true)
+        }
+    }
+
+    checkEditState = (editState) => {
+        if (editState) {
+            this.setState({isOpen : true})
+        } else {
+            this.setState({isOpen : false})
+            this.props.callBackFn()
+        }
+    }
+
+    render() {
+        let editState = this.editState()
+        return(
+            <div>
+                <Dialog open={this.state.isOpen}>
+                    <DialogTitle>Error</DialogTitle>
+                    <DialogContent>You have unsaved changes, please save changes before 
+                    closing this dialog
+                    </DialogContent>
+                    <DialogActions>
+                        <Button raised onClick={() => this.setState({isOpen : false})}>Close</Button>
+                    </DialogActions>
+                </Dialog>
+
+                <Button raised onClick={() => this.checkEditState(editState)}>Close</Button>
+            </div>
+        )
+    }
+}
+
 function TextBoxToggle(props) {
     return(
         <div></div>
     )
 }
 
-function Edit(props) {
-    const [editState, setEditState] = React.useState(false)
+// issue here
+class Edit extends React.Component {
+    constructor(props) {
+        super(props)
+    }
 
-    return(
-        <div>
-            {props.section}{" "}
-            <div className="right-align"><Button onClick={() => props.callBackFn(props.section)}>Edit</Button></div>
-        </div>
-    )
+    buttonLabel = () => {
+        if(this.props.editState) {
+            return ("Save")
+        } else {
+            return ("Edit")
+        }
+    }
+    render() {
+        return(
+            <div>
+                {this.props.section}{" "}
+                <div className="right-align"><Button onClick={() => this.props.callBackFn(this.props.section)}>{this.buttonLabel()}</Button></div>
+            </div>
+        )
+    }
 }
 
 export default Profile
