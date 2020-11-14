350e6d1df662b75ac07b07b69045215fc97da639
Gabriel <yug13@mcmaster.ca>
2020-11-13 21:46:16 -0500

    top bar, settings and notifications

--- src/components/topbar/topbar.jsx
+++ src/components/topbar/topbar.jsx
@@ -2,13 +2,20 @@ import React from "react"
 
 //import '@rmwc/button/styles';
 import { Button } from '@rmwc/button';
+import { SimpleMenuSurface, Menu, MenuItem, SimpleMenu, MenuSurface, MenuSurfaceAnchor } from '@rmwc/menu';
 import { IconButton } from '@rmwc/icon-button';
+import { Select } from '@rmwc/select';
+import { Switch } from '@rmwc/switch';
+import { Radio } from '@rmwc/radio';
+import { List, ListItem, ListItemMeta } from '@rmwc/list';
+import { TopAppBar, TopAppBarRow, TopAppBarSection, TopAppBarTitle, TopAppBarActionItem } from '@rmwc/top-app-bar';
+
 import './topbar.css';
 
 class TopBar extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {showNotif: false, showSettings: false};
+      this.state = {showNotif: false, showSettings: false, notifMute: false, notifMessage: false, notifSound: "Jingle", friendNotif: false, friendMessage: false, friendSound: "Jingle", theme:"Red"};
     }
 
     toggleNotif() { 
@@ -29,38 +36,224 @@ class TopBar extends React.Component {
         console.log(this.state.showSettings);
     }
 
+    offNotif(){
+        this.setState({
+            showNotif: false
+        });
+    }
+
+    offSettings(){
+        this.setState({
+            showSettings: false
+        });
+    }
+
     render() {
         return (
             <div>
-               <article>
-                   EZ Scheduler 4:00
-                   <IconButton id = "settings" icon="settings" style={{color: 'grey', float: 'right', margin: 0}} onClick={this.toggleSettings.bind(this)}/>
-                   <IconButton id = "notif" icon="notifications" style={{color: 'grey', float: 'right', margin: 0}} onClick={this.toggleNotif.bind(this)}/>
-               </article>
-               {this.state.showNotif ? 
-                 <Notif
-                    text='Click "Close Button" to hide popup'
-                    closePopup={this.toggleNotif.bind(this)}
-                 /> : null
-            }
+                <TopAppBar>
+                    <TopAppBarRow>
+                        <TopAppBarSection alignStart>
+                            <TopAppBarTitle>EZ Scheduler 12:06</TopAppBarTitle>
+                        </TopAppBarSection>
+                        <TopAppBarSection alignEnd>
+                            <MenuSurfaceAnchor>
+                            <TopAppBarActionItem  icon="notifications" onClick={this.toggleNotif.bind(this)}/>
+                                <MenuSurface className="notifsSurface" anchorCorner={'bottomStart'} open={this.state.showNotif} onClose={this.offNotif.bind(this)}>
+                                    <Notif/>
+                                </MenuSurface>
+                            </MenuSurfaceAnchor>
+                            <MenuSurfaceAnchor>
+                            <TopAppBarActionItem  className="settingsIcon" icon="settings" onClick={this.toggleSettings.bind(this)}/>
+                                <MenuSurface className="settingsSurface" anchorCorner={'bottomStart'} open={this.state.showSettings} onClose={this.offSettings.bind(this)}>
+                                    <Settings/>
+                                </MenuSurface>
+                            </MenuSurfaceAnchor>
+                        </TopAppBarSection>
+                    </TopAppBarRow>
+                </TopAppBar>
             </div>
         )
     }
 }
 
 class Notif extends React.Component {
-  render() {  
-    return (  
-        <div className='popup'>  
-            <div className='popup\_inner'>  
-                <h1>{this.props.text}</h1>  
-                <button onClick={this.props.closePopup}>close me</button>  
+    render() {  
+        return (  
+            <div style={{ padding : '1px', width: '350px', height: '600px' }}>
+            <a class = "title"><b>Notifications</b></a>
+            <hr/>
+                <List class = "list">
+                    <ListItem>
+                        George wants to be your friend!                        
+                        <ListItemMeta icon="close" />
+                    </ListItem>
+                    <ListItem>
+                        ENG 1D04 Midterm soon!
+                        <ListItemMeta icon="close" />
+                    </ListItem>
+                    <ListItem>Cookes
+                        
+                        <ListItemMeta icon="close" />
+                    </ListItem>
+                </List>
             </div>  
-        </div>  
+        );  
+    }  
+}  
+
+class Settings extends React.Component {    
+    constructor(props) {
+        super(props);
+        this.state = {notifMute: false, notifMessage: false, notifSound: "Jingle", friendNotif: false, friendMessage: false, friendSound: "Jingle", theme:"Red"};
+    }
+
+    setTheme(t){
+        console.log("theme");
+        console.log(this.state.theme);
+        this.setState({
+            theme: t
+        });
+    }
+
+    setfriendSound(s){
+        console.log("friendSound");
+        console.log(this.state.friendSound);
+        this.setState({
+            friendSound: s
+        });
+    }
+
+    setnotifSound(s){
+        console.log("notifSound");
+        console.log(this.state.notifSound);
+        this.setState({
+            notifSound: s
+        });
+    }
+
+    toggleMute(){
+        console.log("Mute");
+        this.setState({
+            notifMute: !this.state.notifMute
+        });
+        console.log(this.state.notifMute);
+    }
+
+    togglenotifMessage(){
+        console.log("notif Message");
+        this.setState({
+            notifMessage: !this.state.notifMessage
+        });
+        console.log(this.state.notifMessage);
+    }
+
+    togglefriendMessage(){
+        console.log("Friend Message");
+        this.setState({
+            friendMessage: !this.state.friendMessage
+        });
+        console.log(this.state.friendMessage);
+    }
+
+    togglefriendNotif(){
+        console.log("friend Notif");
+        this.setState({
+            friendNotif: !this.state.friendNotif
+        });
+        console.log(this.state.friendNotif);
+    }
+
+
+    render() {  
+        return (  
+            <div style={{ padding : '1px', width: '340px', height: '500px' }}>
+                <a class = "title"><b>Settings</b></a>
+                <hr/>
+                <a class = "subtitle">Notifications</a>
+                {/* Mute all button*/}
+                <a class = "switchLine">
+                    <a class = "switchText">Mute All Notifications </a>
+                    <Switch class = "switchButton" defaultNotChecked onChange={this.toggleMute.bind(this)}/>
+                </a>
+                {/* Message Notifications*/}
+                <a class = "switchLine">
+                    <a class = "switchText">Message Notifications </a>
+                    <Switch class = "switchButton" defaultNotChecked  onChange={this.togglenotifMessage.bind(this)} disabled = {this.state.notifMute}/>
+                </a>
+                {/* Message Notifications Sound Drop Down*/}
+                <a class = "dropdownLine">Message Notifications Sound
+                    <Select class = "dropdown" label = "options" enhanced onChange={e => this.setnotifSound(e.target.value)} disabled = {this.state.notifMute}
+                        options = {['Jingle','Ringle','Mingle','Woomp']}
+                    />
+                </a>
+                {/* Friend Request Notifications*/}
+                <a class = "switchLine">
+                    <a class = "switchText">Friend Request Notifcations </a>
+                    <Switch class = "switchButton" defaultNotChecked onChange={this.togglefriendMessage.bind(this)} disabled = {this.state.notifMute}/>
+                </a>
+                {/* Friend Request Notifications*/}
+                <a class = "switchLine">
+                    <a class = "switchText">Message Notifications </a>
+                    <Switch class = "switchButton" defaultNotChecked onChange={this.togglefriendNotif.bind(this)} disabled = {this.state.notifMute}/>
+                </a>
+                {/* Friend Request Notifications Sound DropDown*/}
+                <a class = "dropdownLine">Friend Request Notifications Sound
+                    <Select class = "dropdown" label = "options"  enhanced  onChange={e => this.setfriendSound(e.target.value)} disabled = {this.state.notifMute}
+                        options = {['Jingle','Ringle','Mingle','Woomp']}
+                    />
+                </a>
+                <hr/>
+                {/*Theme Radio Buttons*/}
+                <a class = "subtitle">Theme</a>
+                <a class = "radioLine">
+                <Radio
+                    label="Red"
+                    value="Red"
+                    name="myRadioGroup"
+                    onChange={e => this.setTheme(e.target.value)}
+                />
+                <Radio
+                    label="Blue"
+                    value="Blue"
+                    name="myRadioGroup"
+                    onChange={e => this.setTheme(e.target.value)}
+                />
+                <Radio
+                    label="Yellow"
+                    value="Yellow"
+                    name="myRadioGroup"
+                    onChange={e => this.setTheme(e.target.value)}
+                />
+                </a>
+                <a class = "radioLine">
+                <Radio
+                    label="Green"
+                    value="Green"
+                    name="myRadioGroup"
+                    onChange={e => this.setTheme(e.target.value)}
+                />
+                <Radio
+                    label="Orange"
+                    value="Orange"
+                    name="myRadioGroup"
+                    onChange={e => this.setTheme(e.target.value)}
+                />
+                <Radio
+                    label="Purple"
+                    value="Purple"
+                    name="myRadioGroup"
+                    onChange={e => this.setTheme(e.target.value)}
+                />
+                </a>
+                <hr/>
+                <a class = "buttonLine">
+                    <Button label = "sign out"/>
+                </a>
+            </div>
         );  
     }  
 }  
 
 export { TopBar, TopBar as default }
-
 
